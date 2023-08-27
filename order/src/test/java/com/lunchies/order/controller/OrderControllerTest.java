13f3b1adf48ed5315ec1daa37c3b5a8b2a5e3804
Pedro Caleia <pmig.caleia@gmail.com>
2023-08-27 17:14:30 +0100

    Add the missing basic tests

--- order/src/test/java/com/lunchies/order/controller/OrderControllerTest.java
+++ order/src/test/java/com/lunchies/order/controller/OrderControllerTest.java
@@ -1,5 +1,8 @@
 package com.lunchies.order.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -52,6 +55,67 @@ public class OrderControllerTest {
 		String expectedResponse = "{\"id\": 1}";
 		Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
 		JSONAssert.assertEquals(expectedResponse, result.getResponse().getContentAsString(), false);
+	}
+	
+	@Test
+	public void getOrder() throws Exception  {
+		Order order = new Order("pedrocaleia", 1, 2, 3);
+		order.setId(1);
+		
+		Mockito.when(this.orderService.getOrderById(1)).thenReturn(Optional.of(order));
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/order/1")
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		
+		String expectedResponse = "{\"employee\": \"pedrocaleia\",\"entry\": 1,\"mainCourse\": 2,\"beverage\": 3}";
+		Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+		JSONAssert.assertEquals(expectedResponse, result.getResponse().getContentAsString(), false);
+	}
+	
+	@Test
+	public void listProducts() throws Exception  {
+		Order order1 = new Order("pedrocaleia", 1, 2, 3);
+		order1.setId(1);
+		Order order2 = new Order("pedrocaleia", 4, 5, 6);
+		order2.setId(2);
+		
+		Mockito.when(this.orderService.listOrders()).thenReturn(List.of(order1, order2));
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/order")
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		
+		String expectedResponse = "[{\"employee\": \"pedrocaleia\",\"entry\": 1,\"mainCourse\": 2,\"beverage\": 3},"
+				+ "{\"employee\": \"pedrocaleia\",\"entry\": 4,\"mainCourse\": 5,\"beverage\": 6}]";
+		Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+		JSONAssert.assertEquals(expectedResponse, result.getResponse().getContentAsString(), false);
+	}
+	
+	@Test
+	public void deleteProduct() throws Exception  {
+		Order order = new Order("pedrocaleia", 1, 2, 3);
+		order.setId(1);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.delete("/order/1")
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		
+		String expectedResponse = "";
+		Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+		Assertions.assertEquals(expectedResponse, result.getResponse().getContentAsString());
 	}
 
 }
